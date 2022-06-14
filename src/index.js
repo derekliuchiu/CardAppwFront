d9c4077e05eb2281d732c087521a8646ab0701ba
Derek Liu <derekliuchiu@berkeley.edu>
2022-06-14 14:27:22 -0700

    first

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Amplify from 'aws-amplify';
+import awsExports from "./aws-exports";
+const config = require('./config.json');
+
+Amplify.configure({
+    Auth:
+    {
+        mandatorySignId: true,
+        region: config.cognito.REGION,
+        userPoolId: config.cognito.USER_POOL_ID,
+        userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    }
+});
+
+Amplify.configure(awsExports);
+
+
+ReactDOM.render(<React.StrictMode> <App /> </React.StrictMode>, document.getElementById('root'));
